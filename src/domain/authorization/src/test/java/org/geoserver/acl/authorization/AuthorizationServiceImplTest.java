2540f51bd8bfde635a89f27feb422fde660a1ccd
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2023-03-23 08:14:27 -0300

    Fix (c) headers mentioning original LGPL instead of GPL

--- src/domain/authorization/src/test/java/org/geoserver/acl/authorization/AuthorizationServiceImplTest.java
+++ src/domain/authorization/src/test/java/org/geoserver/acl/authorization/AuthorizationServiceImplTest.java
@@ -2,7 +2,7 @@
  * This code is licensed under the GPL 2.0 license, available at the root
  * application directory.
  *
- * Original from GeoFence 3.6 under LGPL 2.0 license
+ * Original from GeoFence 3.6 under GPL 2.0 license
  */
 
 package org.geoserver.acl.authorization;
