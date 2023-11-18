f98cd865dca3f8a8b333166ec2dbf5a38f54a39a
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2023-11-17 21:15:34 -0300

    Simplify springdoc config, peg to /acl/swagger-ui/index.hml

--- src/artifacts/api/src/main/java/org/geoserver/acl/autoconfigure/springdoc/SpringDocAutoConfiguration.java
+++ src/artifacts/api/src/main/java/org/geoserver/acl/autoconfigure/springdoc/SpringDocAutoConfiguration.java
@@ -4,8 +4,6 @@
  */
 package org.geoserver.acl.autoconfigure.springdoc;
 
-import org.springdoc.core.customizers.OpenApiCustomiser;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
@@ -14,26 +12,7 @@ import org.springframework.context.annotation.Bean;
 public class SpringDocAutoConfiguration {
 
     @Bean
-    SpringDocHomeRedirectController homeController(
-            @Value("${server.servlet.context-path:}") String contextPath) {
+    SpringDocHomeRedirectController homeController() {
         return new SpringDocHomeRedirectController("/swagger-ui/index.html");
-    }
-
-    @Bean
-    public OpenApiCustomiser contextPathCustomiser(
-            @Value("${server.servlet.context-path:/}") String contextPath) {
-        return openApi -> {
-            if (!"/".equals(contextPath)) {
-                openApi.getServers().stream()
-                        .forEach(
-                                server -> {
-                                    final String serverUrl = server.getUrl();
-                                    if (!serverUrl.endsWith(contextPath)) {
-                                        final String newServerUrl = serverUrl + contextPath;
-                                        server.setUrl(newServerUrl);
-                                    }
-                                });
-            }
-        };
     }
 }
